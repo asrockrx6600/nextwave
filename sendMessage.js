6d5c21fce98ef36bbe14b6146154fa006722ecc4
asrockrx6600 <asrockrx6600@gmail.com>
2025-11-28 18:46:05 -0300

    Add files via upload

---
+++ sendMessage.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('form')
+const message = document.getElementById('msg')
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    message.classList.add('active');
+
+    setTimeout(() => {
+        message.classList.remove('active')
+    }, 3000);
+})
+
+const number = '7588439555'
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    const name = document.getElementById('name').value
+    const msg = document.getElementById('message').value.replace(/\n/g, '%0A')
+    const email = document.getElementById('email').value
+
+    const txt = `Olá, meu nome é ${name}.%0A Gostaria de tirar uma dúvida! ${msg}. o meu e-mail para contato é: ${email}.`
+
+    const url = `https://wa.me/${number}?text=${txt}`
+
+    setTimeout(() => {
+        window.open(url, '_blank')
+    }, 1000);
+});
