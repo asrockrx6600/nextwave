bbf099424b57a0e931d5154ac18bcfbf44bd1e2c
asrockrx6600 <asrockrx6600@gmail.com>
2025-11-29 21:10:05 -0300

    Add files via upload

--- sendMessage.js
+++ sendMessage.js
@@ -3,6 +3,7 @@ const nameInput = document.getElementById('name')
 const emailInput = document.getElementById('email')
 const phoneInput = document.getElementById('phone')
 const msgAtention = document.getElementById('msg')
+const message = document.getElementById('message')
 const btn = document.getElementById('btnSend')
 const number = '75981535154'
 
@@ -10,42 +11,31 @@ const number = '75981535154'
 btn.addEventListener('click', (e) => {
     e.preventDefault()
 
-    const msgReturn = msg.value
+    const name = nameInput.value.trim()
+    const email = emailInput.value.trim()
+    const phone = phoneInput.value.trim()
+    const msgReturn = message.value.trim()
 
-    const txt = `Olá, meu nome é ${nameInput}.%0A Gostaria de tirar uma dúvida! ${msgReturn}. O meu e-mail para contato é: ${emailInput}.`
+    if (!name || !email || !phone) {
+        msgAtention.classList.add('active')
+        msgAtention.textContent = 'Preencha os dados'
+        msgAtention.style.color = 'red'
+        return
+    }
+
+    const txt = `Olá, meu nome é ${name}.%0A Gostaria de tirar uma dúvida! ${msgReturn}. O meu e-mail para contato é: ${email}.`
 
     const url = `https://wa.me/${number}?text=${txt}`
 
-    if (nameInput.value === '' || emailInput.value === '' || phoneInput.value === '') {
-        msgAtention.classList.add('active')
-        msgAtention.textContent = 'Preencha os campos!'
-
-    } else {
-        setTimeout(() => {
-            msgAtention.classList.remove('active')
-            window.open(url, '_blank')    
-        }, 2000);  
-    }
-
-    const nameReturn = nameInput.value.trim()
-    const emailReturn = emailInput.value.trim()
-
     msgAtention.classList.add('active')
-
-    if (nameReturn === '' || emailReturn === '' || phoneInput === '') {
-        msgAtention.textContent = 'Preencha os campos!'
-        msgAtention.style.color = 'red'
-
-    } else if (nameInput.value && emailInput.value && phoneInput.value) {
-        msgAtention.textContent = 'Enviado!'
-        msgAtention.style.color = 'green'
-    }
+    msgAtention.textContent = 'Enviado!'
+    msgAtention.style.color = 'green'
 
     setTimeout(() => {
         msgAtention.classList.remove('active')
-    }, 5000);
+        window.open(url, '_blank')
+    }, 2000);
 
-    form.reportValidity()
     form.reset()
 
 })
